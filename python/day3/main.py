5f7d699627a057e72e1d9e410e7116db6383dcf9
Tyler Daines <tyler.daines@edgecast.com>
2022-12-06 22:43:52 -0800

    Day 3 part 2

--- python/day3/main.py
+++ python/day3/main.py
@@ -1,20 +1,26 @@
 def main():
     priority_sum = 0
+    lines = []
 
     with open('input.txt') as f:
         for line in f:
-            mid = int(len(line) / 2)
-            first_half = set(line[:mid])
-            second_half = set(line[mid:])
+            lines.append(line[:-1])
 
-            common = first_half.intersection(second_half).pop()
-            common = ord(common)
-            if ord('a') <= common <= ord('z'):
-                priority = common - ord('a') + 1
-            else:
-                priority = common - ord('A') + 27
+            if len(lines) == 3:
+                first = set(lines[0])
+                second = set(lines[1])
+                third = set(lines[2])
 
-            priority_sum += priority
+                common = (first & second & third).pop()
+                common = ord(common)
+
+                if ord('a') <= common <= ord('z'):
+                    priority = common - ord('a') + 1
+                else:
+                    priority = common - ord('A') + 27
+
+                priority_sum += priority
+                lines.clear()
 
     print(f'Total priority: {priority_sum}')
 
